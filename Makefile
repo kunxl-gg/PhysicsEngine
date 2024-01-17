b8e5d7fb39344ef9c29025ba8c41f282a0185a9e
kunxl-gg <tiwari.25@iitj.ac.in>
2024-01-17 18:51:54 +0530

    Fixed minor bugs
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- Makefile
+++ Makefile
@@ -4,8 +4,8 @@ C_FILES = ./src/*.cpp
 GLAD_FILE = ./src/glad.c
 
 APP_DEFINES :=
-APP_INCLUDES:= -I/usr/local/include -framework Cocoa -framework OpenGL -framework IOKit
+APP_INCLUDES:= -I/usr/local/include/ -framework Cocoa -framework OpenGL -framework IOKit
 APP_LINKERS:= -L/usr/local/lib/ -lglfw3 -v
 
 build:
-	clang++ $(C_FILES) $(GLAD_FILE) -o $(BUILD_DIR)/$(APP_NAME) $(APP_INCLUDES) $(APP_LINKERS)
+	clang++ $(C_FILES) $(GLAD_FILE) -o $(BUILD_DIR)/$(APP_NAME) $(APP_INCLUDES) $(APP_LINKERS) 
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,25 @@
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-
 int main() {
-	if (!glfwInit()) {
-		std::cout << "Failed to initialize GLFW" << std::endl;
-		return -1;
-	}  
+  if (!glfwInit()) {
+    std::cout << "Failed to initialize GLFW" << std::endl;
+    return -1;
+  }
 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window) {
-		std::cout << "Failed to create window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	
-	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
+  GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window) {
+    std::cout << "Failed to create window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
 
-	while (!glfwWindowShouldClose(window)) {
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+  glfwMakeContextCurrent(window);
 
-	glfwTerminate();
-	return 0;
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
